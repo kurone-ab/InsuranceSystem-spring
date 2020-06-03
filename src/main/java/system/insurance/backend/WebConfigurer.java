19eb7f0bbb9888b3decba026c8114530ec93e340
ygnaiih1680 <ygnaiih1680@mju.ac.kr>
2020-06-03 13:39:40 +0900

    restart

---
+++ src/main/java/system/insurance/backend/WebConfigurer.java
@@ -0,0 +1,13 @@
+package system.insurance.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowCredentials(true);
+    }
+}
